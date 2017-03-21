dd56c3b6db22333592b5ebe39fb405a5bfb91fc3
Damtab54 <d>
2017-03-21 09:04:18 +0000

    Modification plan

--- www/plan/plan.controller.js
+++ www/plan/plan.controller.js
@@ -3,13 +3,8 @@
 
 	angular.module('starter.plan', [])
 
-	.controller('PlanCtrl', function($scope) {
-
-		$scope.items = [
-		    { id: 0, title: 'Tokyo', date: 'Septembre 2015'},
-		    { id: 1, title: 'New-York', date: 'Juillet 2016'}
-		    ];
-		console.log("PlanCtrl");
+	.controller('PlanCtrl', function($scope, TripService) {
+		$scope.trips = TripService.all();
 	});
 
 })();
