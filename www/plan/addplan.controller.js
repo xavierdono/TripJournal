2d3985230307883496f37d40201611cede9d2ad3
Damtab54 <d>
2017-03-29 13:58:56 +0100

    Modification d'ajout de plan

--- www/plan/addplan.controller.js
+++ www/plan/addplan.controller.js
@@ -3,10 +3,12 @@
 
   angular.module('starter.addplan', [])
 
-  .controller('AddPlanCtrl', function ($scope, $stateParams, PlanService) {
+  .controller('AddPlanCtrl', function ($scope, $stateParams, PlanService, FileService, ImageService) {
   		$scope.addPlan = function (plan) {
         	PlanService.add($stateParams.tripId, plan.comment);
       		window.location.href = '#/tab/plan.show/'+$stateParams.tripId;
     	}
+
+    	$scope.images = FileService.images();
   });
 })();
